bfe0cd4945362587f531c2895ab40585bfe6a793
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-19 14:23:52 -0700

    Create acmeProgram.cs

---
+++ acmeProgram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcomme to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.Read();
+    }
+}
