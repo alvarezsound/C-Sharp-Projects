447d6ee07cb6a1eac5bd9581912df7e7189ba431
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-25 15:14:30 -0700

    Added loop assignment

---
+++ Basic_C#_Programs/LoopAssignment/LoopAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0; // initialize
+
+            //while (true) // while statement
+            //{
+            //    Console.WriteLine("i = {0}", i); // display "i = "in the console
+            //    i++; // increments i by one
+
+            //    if (i > 20) // continutes the loop and increments until statement is true.
+            //        break;
+            //}
+
+            do // do while statement
+            {
+                Console.WriteLine("i = {0}", i); // display "i = " in the console
+                i++; // increments i by one
+
+
+            } while (i < 20); // continutes the loop and increments until statement is false.
+        }
+    }
+}
