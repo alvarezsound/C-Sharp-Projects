77677b1e3f89f33942469100322b24af73856346
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-25 15:33:06 -0700

    Added string assignment

---
+++ Basic_C#_Programs/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello, ";
+            string fName = "Jeff ";
+            string lName = "Alvarez";
+            string all = greeting + fName + lName;
+            Console.WriteLine(all.ToUpper());
+
+        }
+    }
+}
