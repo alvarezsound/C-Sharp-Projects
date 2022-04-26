40bcd06272e555d2436377254a84ece68ad268e6
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-26 15:09:19 -0700

    Added calling methods assignment

---
+++ Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/Operator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace CallingMethodsAssignment
+{
+    public class Operator
+    {
+        public int Add(int data) // add method
+        {
+            return data + 4;
+        }
+
+        public int Subtract(int data) // subtract method
+        {
+            return data - 2;
+        }
+
+        public int Divide(int data) // divide method
+        {
+            return data / 2; 
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine(); // user inputs a number as string
+            Data = Int32.Parse(input); // convert string to int for operations
+            var operatorObject = new Operator(); // create a new object from the Operator class
+            Console.WriteLine(Data + " + 4 =");
+            var resultOne = operatorObject.Add(Data); // call the Add method from Operator.cs
+            Console.WriteLine(resultOne); // display result to console
+            Console.WriteLine(Data + " - 2 =");
+            var resultTwo = operatorObject.Subtract(Data); // call the Subtract method from Operator.cs
+            Console.WriteLine(resultTwo); // display result to console
+            Console.WriteLine(Data + " / 2 =");
+            var resultThree = operatorObject.Divide(Data); // call the Divide method from Operator.cs
+            Console.WriteLine(resultThree); // display result to console
+            Console.ReadLine();
+        }
+    }
+}
