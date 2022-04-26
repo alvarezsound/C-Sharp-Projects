3bdc4e09af8649bd314103c9e90d317488f15a49
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-26 16:11:22 -0700

    Added Main Method Assignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    public class Operation
+    {
+        public int Math(int data) // method that takes in integer
+        {
+            return data + 4;
+        }
+
+        public double Math(double data) // method that takes in double/decimals
+        {
+            return data * 2.5;
+        }
+
+        public string Math(string data) // method that takes in strings
+        {
+            int myData = Convert.ToInt32(data); // converts string to int
+            int result = myData / 4;
+            return result.ToString(); // converts result back to string
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 80;
+            var objectOne = new Operation(); // instantiate the Operation class
+            var resultOne = objectOne.Math(myNumber); // Call the method from Operation.cs and pass in the integer
+            Console.WriteLine(resultOne);
+
+            double myDecimal = 20.5;
+            var objectTwo = new Operation(); // instantiate the Operation class
+            var resultTwo = objectTwo.Math(myDecimal); // Call the method from Operation.cs and pass in the double/decimal
+            Console.WriteLine(resultTwo);
+
+            string myString = "40";
+            var objectThree = new Operation(); // instantiate the Operation class
+            var resultThree = objectThree.Math(myString); // Call the method from Operation.cs and pass in the string
+            Console.WriteLine(resultThree);
+
+            Console.ReadLine();
+        }
+    }
+}
