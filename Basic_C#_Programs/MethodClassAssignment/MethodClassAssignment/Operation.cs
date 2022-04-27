ff5712c63034d02968981abc6a386901273ddf04
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-26 17:43:21 -0700

    Added method class assignment

---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Operation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    class Operation
+    {
+        public void Math(int a, int b)
+        {
+            int sum = a + 4; // add first integerr
+            Console.WriteLine(b); // display second integer
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operatorObject = new Operation(); // instantiate the Operation class
+            operatorObject.Math(4, 8); // call method and passing in two integers
+            operatorObject.Math(a: 20, b: 40); // specifying paramters by name
+            Console.ReadLine();
+        }
+    }
+}
