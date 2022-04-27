09c049086a526120a2b219c5e45dee8ca48974b3
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-27 16:06:57 -0700

    Added methods and objects assignment

---
+++ Basic_C#_Programs/MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Employee : Person
+    {
+        public int id { get; set; } // add an int property
+    }
+}
---
+++ Basic_C#_Programs/MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        public string FirstName { get; set; } // add a string property
+        public string LastName { get; set; } // add a string property
+
+        public void SayName() // method
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
