f8ff8e768d8c5c07148f1abb51a7906243e272f4
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-27 16:12:58 -0700

    Organized folders

---
+++ Basic_C#_Programs/Video Exercises/TwentyOne/TwentyOne/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    public class Game
+    {
+        public List<string> Players { get; set; }
+        public string Name { get; set; }    
+        public string Dealer { get; set; }
+
+        public void SayName()
+        {
+            
+        }
+    }
+}
