7cda41ad924b9eef5745ab63e80aa954ad9c2757
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-26 17:11:51 -0700

    Added method assignment

---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/Operation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment
+{
+    class Operation
+    {
+        public int Math(int dataOne, int dataTwo = 0) 
+            // Method that takes in two integers. The second integer has a default value of 0 in case user does not input a number.
+        {
+            return dataOne + dataTwo + 4;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operatorObject = new Operation(); // instantiate the Operation class
+
+            Console.WriteLine("Pick a number:");
+            int userValue1 = Convert.ToInt32(Console.ReadLine()); // conver users input as string to int
+            Console.WriteLine("Pick another number (optional)");
+
+            int userValue2;
+            int result;
+            if (int.TryParse(Console.ReadLine(), out userValue2)) // if user inputs a second number
+            {
+                result = operatorObject.Math(userValue1, userValue2); // call method for both inputs
+            }
+            else
+            {
+                result = operatorObject.Math(userValue1); // else call method for first input only
+            }
+            Console.WriteLine(userValue1 + " + " + userValue2 + " + 4 = ");
+            Console.WriteLine(result); // displays result to console
+            Console.ReadLine();
+
+        }
+    }
+}
