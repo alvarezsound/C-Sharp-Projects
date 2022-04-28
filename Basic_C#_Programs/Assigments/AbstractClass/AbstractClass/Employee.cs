714499affd7d3016509b0628022b01524c56aa7b
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-27 18:27:16 -0700

    Updated abstract class program with  operator submission assignment

--- Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Employee.cs
+++ Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Employee.cs
@@ -6,14 +6,23 @@ namespace AbstractClass
 {
     public class Employee : Person, IQuittable // inherits from Person Class and IQuittable interface
     {
+        public int ID { get; set; }
         public override void SayName() // implement the SayName method from Person class
         {
             Console.WriteLine("Name: " + FirstName + " " + LastName);
         }
-        public void Quit()
+        public void Quit() // inherited from IQuittable
         {
             Console.WriteLine("The Program will now quit...");
             Environment.Exit(0);
+        }
+        public static bool operator ==(Employee employeeObject, Employee employeeObject2)
+        {
+           return employeeObject.ID == employeeObject2.ID; // overloading == operator
+        }
+        public static bool operator !=(Employee employeeObject, Employee employeeObject2)
+        {
+            return employeeObject.ID != employeeObject2.ID; // overloading != operator
         }
     }
 }
--- Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Program.cs
+++ Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Program.cs
@@ -9,7 +9,17 @@ namespace AbstractClass
             Employee employeeObject = new Employee(); // instantiate an object from Employee class
             employeeObject.FirstName = "Sample"; // add value to string property inherited from Person Class
             employeeObject.LastName = "Student"; // add value to string property inherited from Person Class
+            employeeObject.ID = 14;
             employeeObject.SayName(); // call superclass method
+
+            Employee employeeObject2 = new Employee();
+            employeeObject2.FirstName = "Test"; // add value to string property inherited from Person Class
+            employeeObject2.LastName = "Student"; // add value to string property inherited from Person Class
+            employeeObject2.ID = 33;
+            employeeObject2.SayName(); // call superclass method
+
+            Console.WriteLine("Do the two employees have the same ID?");
+            Console.WriteLine(employeeObject.ID == employeeObject2.ID); // compare the two employee objects by ID
 
             employeeObject.Quit(); // call quit method which is inherited from the IQuittable interface
             Console.ReadLine();
