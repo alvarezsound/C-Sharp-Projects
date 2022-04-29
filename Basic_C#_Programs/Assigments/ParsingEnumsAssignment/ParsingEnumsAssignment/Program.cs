b9234e0b88193036d08e40f1dc4221b4fff40f79
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-29 15:20:54 -0700

    Added parsing enums assignment

---
+++ Basic_C#_Programs/Assigments/ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week.");
+                string userValue = Console.ReadLine();
+
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userValue);
+
+                if (day == DaysOfTheWeek.Friday)
+                {
+                    Console.WriteLine("Today is Friday.");
+                }
+                else
+                {
+                    Console.WriteLine("Today is not " + day);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.Read();
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
