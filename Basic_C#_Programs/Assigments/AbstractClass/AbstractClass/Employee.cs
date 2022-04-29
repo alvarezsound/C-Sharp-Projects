5fed6a082b9f455888086016f149aedab88c3332
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-29 14:47:04 -0700

    Added parameters assignment to abstract class project

--- Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Employee.cs
+++ Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Employee.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace AbstractClass
 {
-    public class Employee : Person, IQuittable // inherits from Person Class and IQuittable interface
+    public class Employee<T> : Person, IQuittable // inherits from Person Class and IQuittable interface
     {
         public int ID { get; set; }
+        public List<T> Things { get; set; } // List property with generic data type
         public override void SayName() // implement the SayName method from Person class
         {
             Console.WriteLine("Name: " + FirstName + " " + LastName);
@@ -16,11 +17,11 @@ namespace AbstractClass
             Console.WriteLine("The Program will now quit...");
             Environment.Exit(0);
         }
-        public static bool operator ==(Employee employeeObject, Employee employeeObject2)
+        public static bool operator ==(Employee<T> employeeObject, Employee<T> employeeObject2)
         {
            return employeeObject.ID == employeeObject2.ID; // overloading == operator
         }
-        public static bool operator !=(Employee employeeObject, Employee employeeObject2)
+        public static bool operator !=(Employee<T> employeeObject, Employee<T> employeeObject2)
         {
             return employeeObject.ID != employeeObject2.ID; // overloading != operator
         }
--- Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Program.cs
+++ Basic_C#_Programs/Assigments/AbstractClass/AbstractClass/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AbstractClass
 {
@@ -6,17 +7,37 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            Employee employeeObject = new Employee(); // instantiate an object from Employee class
+            Employee<string> employeeObject = new Employee<string>(); // instantiate an object from Employee class
             employeeObject.FirstName = "Sample"; // add value to string property inherited from Person Class
             employeeObject.LastName = "Student"; // add value to string property inherited from Person Class
             employeeObject.ID = 14;
             employeeObject.SayName(); // call superclass method
 
-            Employee employeeObject2 = new Employee();
+            Employee<string> employeeObject2 = new Employee<string>();
             employeeObject2.FirstName = "Test"; // add value to string property inherited from Person Class
             employeeObject2.LastName = "Student"; // add value to string property inherited from Person Class
             employeeObject2.ID = 33;
             employeeObject2.SayName(); // call superclass method
+
+
+            List<string> employeeObject3 = new List<string>() // Assign list of strings to Things
+            {
+                "Jim", "Bob", "Sally", "Tom"
+            };
+
+            List<int> employeeObject4 = new List<int>() // Assign list of ints to Things
+            {
+                1, 2, 3, 4, 5, 6
+            };
+
+            foreach (var x in employeeObject3) // loop that prints all string things to console
+            {
+                Console.WriteLine(x);
+            }
+            foreach (var y in employeeObject4) // loop that prints all int things to console
+            {
+                Console.WriteLine(y);
+            }
 
             Console.WriteLine("Do the two employees have the same ID?");
             Console.WriteLine(employeeObject.ID == employeeObject2.ID); // compare the two employee objects by ID
