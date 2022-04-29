829cd8897071555877bb689cf1401b09ed9bf235
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-29 15:42:20 -0700

    Added struct assignment

---
+++ Basic_C#_Programs/Assigments/StructAssignment/StructAssignment/Numbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructAssignment
+{
+    public struct Numbers
+    {
+        public decimal Amount { get; set; } // create property with decimal data type
+    }
+}
---
+++ Basic_C#_Programs/Assigments/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers amount = new Numbers(); // create object with data type Numbers from struct
+            amount.Amount = 15.4368m; // assign amount
+            Console.WriteLine(amount.Amount);
+            Console.ReadLine();
+        }
+    }
+}
