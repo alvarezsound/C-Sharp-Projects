f94e324f7911602039f3e202d34bdf6c204f1687
alvarezsound <jeffalvarezsound@gmail.com>
2022-04-29 18:51:28 -0700

    Added constructor assignment

---
+++ Basic_C#_Programs/Assigments/ConstructorAssignment/ConstructorAssignment/Constructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.cs
+{
+    public class Construct
+    {
+        public string name;
+        public int id;
+
+        public Construct() : this("Jeff", 27)
+        {
+
+        }
+        public Construct(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Assigments/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // const variable
+            const string name = "Jeff";
+
+            // var variable
+            var id = 27;
+
+            // Chain 2 constructors together
+            Construct con1 = new Construct(), con2 = new Construct("Jenna", 21);
+
+            Console.WriteLine(con1.name + " " + con1.id);
+            Console.WriteLine(con2.name + " " + con2.id);
+
+            Console.Read();
+        }
+    }
+}
