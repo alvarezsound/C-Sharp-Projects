6c37506535d0684d30f2b2b43e04af9395d5fa29
alvarezsound <jeffalvarezsound@gmail.com>
2022-05-03 15:57:37 -0700

    Added scores exercise

--- C_Sharp_Exercises/Scores/Scores/Program.cs
+++ C_Sharp_Exercises/Scores/Scores/Program.cs
@@ -12,12 +12,12 @@ namespace Scores
             string msg = $"\nWelcome back {uName}, today is {date}.";
             Console.WriteLine(msg);
 
-            string path = @"";
+            string path = @"\\Mac\Home\Documents\GitHub\C-Sharp-Projects\C_Sharp_Exercises\Scores\Scores\studentScores.txt";
             string[] lines = System.IO.File.ReadAllLines(path);
 
             double tScore = 0.0;
 
-            Console.WriteLine("\nStudent Score:  \n");
+            Console.WriteLine("\nStudent Scores:  \n");
             foreach (string line in lines)
             {
                 Console.Write("\n" + line);
