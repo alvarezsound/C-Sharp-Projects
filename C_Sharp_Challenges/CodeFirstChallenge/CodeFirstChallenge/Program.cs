ec7e96766eb8ba501ca67ffa5e6c269b1d83094a
alvarezsound <jeffalvarezsound@gmail.com>
2022-05-03 18:06:13 -0700

    Added Code First Challenge

---
+++ C_Sharp_Challenges/CodeFirstChallenge/CodeFirstChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var studentContext = new StudentContext())
+            {
+                var student = new Student() { StudentId = 0, StudentFName = "Jeff", StudentLName = "Alvarez", StudentGPA = 3.8 };
+
+                studentContext.Students.Add(student);
+                studentContext.SaveChanges();
+
+                Console.WriteLine("Student successfully added");
+                Console.ReadLine();
+            }
+        }
+    }
+}
